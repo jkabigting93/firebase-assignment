66f86542228db6cec383b45a8c49dad6368adb61
Joey Kabigting <joey.kabigting@mail.utoronto.ca>
2019-10-22 04:58:39 -0400

    wrapped all js in a document ready wrapper

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,54 +1,56 @@
+// Wrapped everything in a document ready wrapper to make sure scripts run only after entire DOM has loaded
+$(document).ready(function() {
+
 // Initialize Firebase
-var firebaseConfig = {
-    apiKey: "AIzaSyDQJ27gQO_jJW-FUJbvkfIpeQ9WX9tjFM8",
-    authDomain: "fir-assignment-2019.firebaseapp.com",
-    databaseURL: "https://fir-assignment-2019.firebaseio.com",
-    projectId: "fir-assignment-2019",
-    storageBucket: "fir-assignment-2019.appspot.com",
-    messagingSenderId: "1077675087519",
-    appId: "1:1077675087519:web:1f5dfc0c19fd31d9afb4aa",
-    measurementId: "G-BPCTSBW4LB"
-};
+    var firebaseConfig = {
+        apiKey: "AIzaSyDQJ27gQO_jJW-FUJbvkfIpeQ9WX9tjFM8",
+        authDomain: "fir-assignment-2019.firebaseapp.com",
+        databaseURL: "https://fir-assignment-2019.firebaseio.com",
+        projectId: "fir-assignment-2019",
+        storageBucket: "fir-assignment-2019.appspot.com",
+        messagingSenderId: "1077675087519",
+        appId: "1:1077675087519:web:1f5dfc0c19fd31d9afb4aa",
+        measurementId: "G-BPCTSBW4LB"
+    };
 
-firebase.initializeApp(firebaseConfig);
+    firebase.initializeApp(firebaseConfig);
+    var database = firebase.database;
 
-// Declare empty variables for user input
-var name = "";
-var destination = "";
-var firstTrainTime = "";
-var frequency = "";
+    // Declare empty variables for user input
+    var name = "";
+    var destination = "";
+    var firstTrainTime = "";
+    var frequency = "";
 
-// Added click listener to grab user input and send to firebase
-$("button").on("click", function() {
-    name = $("#trainNameInput").val().trim();
-    destination = $("#destinationInput").val().trim();
-    firstTrainTime = moment($("#firstTrainTimeInput").val().trim(), "hh:mm").subtract(1, "years").format("X");
-    frequency = $("#frequencyInput").val().trim();
+    // Added click listener to grab user input and send to firebase
+    $("button").on("click", function() {
+        name = $("#trainNameInput").val().trim();
+        destination = $("#destinationInput").val().trim();
+        firstTrainTime = moment($("#firstTrainTimeInput").val().trim(), "hh:mm").subtract(1, "years").format("X");
+        frequency = $("#frequencyInput").val().trim();
 
-    firebase.database().ref().set({
-        name:name,
-        destination:destination,
-        firstTrainTime:firstTrainTime,
-        frequency:frequency
+        firebase.database().ref().set({
+            name:name,
+            destination:destination,
+            firstTrainTime:firstTrainTime,
+            frequency:frequency
+        })
     })
-})
 
-// Calculations to determine calculated variables (next arrival and minutes away) using moment.js
-database.ref().on("child_added", function(childSnapshot) {
-    var nextArrival = "";
-    var minutesAway = "";
-    var firstTrainTimeLastYear = moment(childSnapshot.val().firstTrainTime, "hh:mm").subtract(1, "years");
-    var timeDifference = moment().diff(moment(firstTrainTimeLastYear), "minutes");
-    var remainingTime = timeDifference % childSnapshot.val().frequency;
-    var minutesAway = childSnapshot.val().frequency - remainingTime;
-    var nextArrival = moment().add(minutesAway, "minutes");
-    nextArrival = moment(nextArrival).format("hh:mm");
+    // Calculations to determine calculated variables (next arrival and minutes away) using moment.js
+    firebase.database.ref().on("child_added", function(childSnapshot) {
+        var nextArrival = "";
+        var minutesAway = "";
+        var firstTrainTimeLastYear = moment(childSnapshot.val().firstTrainTime, "hh:mm").subtract(1, "years");
+        var timeDifference = moment().diff(moment(firstTrainTimeLastYear), "minutes");
+        var remainingTime = timeDifference % childSnapshot.val().frequency;
+        var minutesAway = childSnapshot.val().frequency - remainingTime;
+        var nextArrival = moment().add(minutesAway, "minutes");
+        nextArrival = moment(nextArrival).format("hh:mm");
 
-// Appending new entries to table
-    firebase.database().ref().on("value", function(snapshot){
-        var newEntryRow = $("<tr>");
-        var rowContent = "<td>" + name + "</td><td>" + destination + "</td><td>" + frequency + "</td><td>" + nextArrival + "</td><td>" + minutesAway + "</td>";
-    
-        console.log(snapshot.val());
-    })
-})
+    // Appending new entries to table
+        $("tbody").append("<tr><td>" + childSnapshot.val().name + "</td><td>" + destination + "</td><td>" + frequency + "</td><td>" + nextArrival + "</td><td>" + minutesAway + "</td></tr>");
+    }, function(err) {
+        console.log("Error: " + err.code);
+    });
+});
